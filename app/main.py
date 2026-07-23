fafbf165a31ff6aaf2789a56ca477e2ccf7c6cc7
Khushboo Gupta <guptakhushboo0305@gmail.com>
2026-07-23 20:23:57 +0530

    Initial FastAPI project setup

---
+++ app/main.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI,Response,status,HTTPException,Depends 
+from fastapi.params import Body
+from pydantic import BaseModel 
+from typing import Optional,List
+from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from sqlalchemy.orm import Session
+from . import models,schemas,utils
+from .database import engine, get_db
+from .routers import post,user,auth
+
+models.Base.metadata.create_all(bind=engine) 
+
+
+app =  FastAPI()
+
+
+class Post(BaseModel):
+    title:str
+    content:str
+    published: bool= True
+    
+while True:
+    
+    try:
+        conn=psycopg2.connect(host='localhost',database='fastapi',user='postgres',password='khushboo0305',cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("database connection was successful")
+        break
+    except Exception as error:
+        print("connecting to database failed")
+        print ("error was:",error)
+        time.sleep(2)
+    
+my_posts = [{"title":"title of post 1","content": "content of post 1", "id":1},{"title":"favourite foods","content": "I like pizza","id":2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"]== id:
+            return p
+        
+def find_index_post(id):
+    for i,p in enumerate(my_posts):
+        if p['id']==id:
+            return i 
+        
+        
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+#@app.get("/sqlalchemy")
+#def test_posts(db:Session = Depends(get_db)): 
+    
+    #posts = db.query(models.Post).all()
+    #return  post
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ app/schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel,ConfigDict,EmailStr
+from datetime import datetime
+
+class PostBase(BaseModel):
+    title:str
+    content: str
+    published: bool = True
+    
+
+class PostCreate(PostBase):
+    pass 
+
+class Post(PostBase):
+    id : int
+    created_at : datetime
+    
+    model_config = ConfigDict(from_attributes=True)
+    
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    
+class UserOut(BaseModel):
+    id : int
+    email : EmailStr
+    created_at : datetime
+    
+    model_config = ConfigDict(from_attributes=True)
+    
+        
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
